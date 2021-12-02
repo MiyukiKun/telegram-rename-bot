aa4f6460ee884aabfd77d19c457627eee5abcab7
MiyukiKun <miyukibot1@gmail.com>
2021-12-02 22:16:01 +0530

    FastTelethon

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telethon import events
 import os
-from helper import Timer, progress_bar_str
 from config import bot
+from FastTelethonhelper import fast_download, fast_upload
 
 is_busy = False
 
@@ -18,24 +18,17 @@ async def rename(event):
         await event.reply("Im Busy with another process, try again later")
         return
     is_busy == True
-    timer = Timer()
     reply = await bot.send_message(event.chat_id, "Downloading") 
-    async def callbackd(current, total):
-        if timer.can_send():
-            await reply.edit(f"Downloading...\n{progress_bar_str(current, total)}")
-    async def callbacku(current, total):
-        if timer.can_send():
-            await reply.edit(f"Uploading...\n{progress_bar_str(current, total)}")
     try:
         name = event.raw_text.split()
         name.pop(0)
         name = " ".join(name)
         msg = await event.get_reply_message()
-        await bot.download_media(msg, name, progress_callback=callbackd)
-        file = await bot.upload_file(name, progress_callback=callbacku)
+        download_location = await fast_download(bot, msg, reply=reply, download_folder='./downloads/')
+        file = await fast_upload(client=bot, reply=reply, file_location=download_location, name=name)
         await bot.send_message(event.chat_id, file=file, thumb="thumb.png", force_document=True)
         await reply.delete()
-        os.remove(name)
+        os.remove(download_location)
 
     except Exception as e:
         await event.reply(str(e))
@@ -44,47 +37,47 @@ async def rename(event):
         is_busy == False
 
 
-# @bot.on(events.NewMessage(pattern=("/magic")))
-# async def batch_rename(event):
-#     global is_busy
-#     if is_busy == True:
-#         await event.reply("Im Busy with another process, try again later")
-#         return
-#     is_busy == True
-#     data = event.text.split(":")
-#     files = []
-#     try:
-#         for i in range(int(data[1]),int(data[2])+1):
-#             x = await bot.get_messages(event.chat_id, ids=i)
-#             if x is not None:
-#                 files.append(x)
-#         a = int(data[4])
-#         for i in range(len(files)):
-#             name = data[3]
-#             if a<10:
-#                 temp = name.replace("OwO", f"00{a}")
-#                 temp = temp.replace("UwU", f"0{a}")
+@bot.on(events.NewMessage(pattern=("/magic")))
+async def batch_rename(event):
+    global is_busy
+    if is_busy == True:
+        await event.reply("Im Busy with another process, try again later")
+        return
+    is_busy == True
+    data = event.text.split(":")
+    files = []
+    try:
+        for i in range(int(data[1]),int(data[2])+1):
+            x = await bot.get_messages(event.chat_id, ids=i)
+            if x is not None:
+                files.append(x)
+        a = int(data[4])
+        for i in range(len(files)):
+            name = data[3]
+            if a<10:
+                temp = name.replace("OwO", f"00{a}")
+                temp = temp.replace("UwU", f"0{a}")
 
-#             elif a<100:
-#                 temp = name.replace("OwO", f"0{a}")
-#                 temp = temp.replace("UwU", f"{a}")
+            elif a<100:
+                temp = name.replace("OwO", f"0{a}")
+                temp = temp.replace("UwU", f"{a}")
 
-#             else:
-#                 temp = name.replace("OwO", f"{a}")
+            else:
+                temp = name.replace("OwO", f"{a}")
 
-#             reply = await files[i].reply("Downloading...")
-#             download_location = await fast_download(bot, files[i], reply)
-#             file = await fast_upload(client=bot, reply=reply, file_location=download_location, name=temp)
-#             await bot.send_message(event.chat_id, file=file, thumb="thumb.png", force_document=True)
-#             a = a+1
-#             await reply.delete()
-#             os.remove(download_location)
+            reply = await files[i].reply("Downloading...")
+            download_location = await fast_download(bot, files[i], reply)
+            file = await fast_upload(client=bot, reply=reply, file_location=download_location, name=temp)
+            await bot.send_message(event.chat_id, file=file, thumb="thumb.png", force_document=True)
+            a = a+1
+            await reply.delete()
+            os.remove(download_location)
 
-#     except Exception as e:
-#         await event.reply(e)
+    except Exception as e:
+        await event.reply(e)
     
-#     finally:
-#         is_busy == False
+    finally:
+        is_busy == False
 
 
 @bot.on(events.NewMessage(pattern=("/sthumb")))
